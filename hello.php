950c92b82835e1fb21394bb400adce403f034c97
Loris01100 <lolobach01100@gmail.com>
2024-10-28 16:56:27 +0100

    Début (bug jeu)

--- hello.php
+++ hello.php
@@ -1,4 +1,51 @@
 <?php
-echo ("Hello world!");
+$mot = ["Peche", "Camion", "Banane", "Voiture", "Douche", "Gymnastique"];
 
-    ?>
+$motatrouver = strtolower($mot[array_rand($mot)]);
+$reponse = "";
+$motLength = strlen($motatrouver);
+
+echo "Motus\n";
+
+while ($reponse !== $motatrouver) {
+
+    $reponse = strtolower(readline("Mot (" . $motLength . " lettres) : "));
+
+    if (strlen($reponse) != $motLength) {
+        echo "Le mot fait " . $motLength . " lettres.\n";
+        continue;
+    }
+
+    $feedback = "";
+    $lettresMarquees = [];
+
+    for ($i = 0; $i < $motLength; $i++) {
+        if ($reponse[$i] == $motatrouver[$i]) {
+            $feedback .= strtoupper($reponse[$i]);
+            $lettresMarquees[$i] = true;
+        } else {
+            $lettresMarquees[$i] = false;
+        }
+    }
+
+    for ($i = 0; $i < $motLength; $i++) {
+        if (!$lettresMarquees[$i]) {
+            if (strpos($motatrouver, $reponse[$i]) !== false && substr_count($reponse, $reponse[$i]) <= substr_count($motatrouver, $reponse[$i])) {
+
+                $essai .= "(" . $reponse[$i] . ")";
+            } else {
+
+                $essai .= "*";
+            }
+        }
+    }
+
+    echo "Résultat : $feedback\n";
+
+    if ($reponse === $motatrouver) {
+        echo "Bravo ! Vous avez trouvé le mot : " . ucfirst($motatrouver) . "\n";
+        break;
+    } else {
+        echo "Essayez encore.\n";
+    }
+}
