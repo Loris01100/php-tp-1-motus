b69832de6c54a437f75d8369bc6e3499e64f4e90
Loris01100 <lolobach01100@gmail.com>
2024-11-04 16:50:41 +0100

    autoload

--- hello.php
+++ hello.php
@@ -1,51 +1,80 @@
 <?php
-$mot = ["Peche", "Camion", "Banane", "Voiture", "Douche", "Gymnastique"];
 
-$motatrouver = strtolower($mot[array_rand($mot)]);
-$reponse = "";
-$motLength = strlen($motatrouver);
+require_once __DIR__ . '/vendor/autoload.php';
+function MotAleatoire($words)
+{
+    return strtolower($words[array_rand($words)]);
+}
 
-echo "Motus\n";
-
-while ($reponse !== $motatrouver) {
-
-    $reponse = strtolower(readline("Mot (" . $motLength . " lettres) : "));
-
-    if (strlen($reponse) != $motLength) {
-        echo "Le mot fait " . $motLength . " lettres.\n";
-        continue;
+function choixUti($longeur)
+{
+    while (true) {
+        $response = strtolower(readline("Mot (" . $longeur . " lettres) : "));
+        if (strlen($response) == $longeur) {
+            return $response;
+        }
+        echo "Le mot fait " . $longeur . " lettres.\n";
     }
+}
 
+// Function to give feedback on the guessed word
+function getFeedback($response, $targetWord)
+{
     $feedback = "";
-    $lettresMarquees = [];
+    $markedLetters = [];
+    $wordLength = strlen($targetWord);
 
-    for ($i = 0; $i < $motLength; $i++) {
-        if ($reponse[$i] == $motatrouver[$i]) {
-            $feedback .= strtoupper($reponse[$i]);
-            $lettresMarquees[$i] = true;
+    // Check for correct letters in the correct positions
+    for ($i = 0; $i < $wordLength; $i++) {
+        if ($response[$i] == $targetWord[$i]) {
+            $feedback .= strtoupper($response[$i]);
+            $markedLetters[$i] = true;
         } else {
-            $lettresMarquees[$i] = false;
+            $markedLetters[$i] = false;
         }
     }
 
-    for ($i = 0; $i < $motLength; $i++) {
-        if (!$lettresMarquees[$i]) {
-            if (strpos($motatrouver, $reponse[$i]) !== false && substr_count($reponse, $reponse[$i]) <= substr_count($motatrouver, $reponse[$i])) {
-
-                $essai .= "(" . $reponse[$i] . ")";
+    // Check for correct letters in incorrect positions
+    for ($i = 0; $i < $wordLength; $i++) {
+        if (!$markedLetters[$i]) {
+            if (strpos($targetWord, $response[$i]) !== false && substr_count($response, $response[$i]) <= substr_count($targetWord, $response[$i])) {
+                $feedback .= "(" . $response[$i] . ")";
             } else {
-
-                $essai .= "*";
+                $feedback .= "*";
             }
         }
     }
 
-    echo "Résultat : $feedback\n";
+    return $feedback;
+}
 
-    if ($reponse === $motatrouver) {
-        echo "Bravo ! Vous avez trouvé le mot : " . ucfirst($motatrouver) . "\n";
-        break;
-    } else {
-        echo "Essayez encore.\n";
+// Function to play the game
+function playMotus($words)
+{
+    $targetWord = MotAleatoire($words);
+    $wordLength = strlen($targetWord);
+
+    echo "Motus\n";
+
+    while (true) {
+        $response = choixUti($wordLength);
+        $feedback = getFeedback($response, $targetWord);
+
+        echo "Résultat : $feedback\n";
+
+        if ($response === $targetWord) {
+            echo "Bravo ! Vous avez trouvé le mot : " . ucfirst($targetWord) . "\n";
+            break;
+        } else {
+            echo "Essayez encore.\n";
+        }
     }
 }
+
+// Array of words for the game
+$words = ["Peche", "Camion", "Banane", "Voiture", "Douche", "Gymnastique"];
+
+// Start the game
+playMotus($words);
+
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '3olen/tp-motus',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '950c92b82835e1fb21394bb400adce403f034c97',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '3olen/tp-motus' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '950c92b82835e1fb21394bb400adce403f034c97',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
